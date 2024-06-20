28358d13f36c3b032e90ac31ee5c609beb4413c9
Mitch Toran <mitchellht34@gmail.com>
2024-06-20 15:39:23 -0400

    complete lab

---
+++ src/context/user.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+    // const currentUser = {
+    //     name: 'Duane',
+    //     interests: ['Coding', 'Biking', "Words ending in 'ing'"],
+    // };
+    const [user, setUser] = useState(null);
+    
+    // the value prop of the provider will be our context date
+    // this value will be available to child components of this provider
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+          {children}
+        </UserContext.Provider>
+      );
+}
+
+export { UserContext, UserProvider };
